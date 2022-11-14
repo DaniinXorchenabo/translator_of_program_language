a5db5f5ce647e62dad2a37e361b6b841bf034dc6
DaniinXorchenabo <rkbcu@mail.ru>
2022-11-14 11:07:59 +0300

    :bug: Для терминальных и нетерменальных множеств изменены функции равенства для сравнения объектов подклассов этих множеств.

--- syntax_analyzer/lexical/no_terminals.py
+++ syntax_analyzer/lexical/no_terminals.py
@@ -5,6 +5,8 @@ __all__ = [
     'no_terminals_dict'
 ]
 
+from typing import Type
+
 no_terminals_dict = dict(
     tM='Start_state',
     tD="Define_all_variables_with_syntax",
@@ -25,6 +27,25 @@ no_terminals_dict = dict(
     tZ = 'tZ',
 )
 
+def eq_enum_decorator(default_eq):
+    def no_terminals_eq(self, item):
+        global _first_NO_TERMINALS
+        if _first_NO_TERMINALS and isinstance(item, _first_NO_TERMINALS) :
+            return item.name == self.name and item.value == self.value
+        else:
+            return default_eq(self, item)
+
+    return no_terminals_eq
+
+def ne_enum_decorator(default_ne):
+    def no_terminals_no_eq(self, item):
+        global _first_NO_TERMINALS
+        if _first_NO_TERMINALS and isinstance(item, _first_NO_TERMINALS) :
+            return item.name != self.name or item.value != self.value
+        else:
+            return default_ne(self, item)
+
+    return no_terminals_no_eq
 
 def get_no_terminals():
     global _NO_TERMINALS, _first_NO_TERMINALS, no_terminals_dict
@@ -33,6 +54,8 @@ def get_no_terminals():
     if _NO_TERMINALS is None:
         _NO_TERMINALS = NO_TERMINALS
         _first_NO_TERMINALS = NO_TERMINALS
+        _first_NO_TERMINALS.__eq__ = eq_enum_decorator(_first_NO_TERMINALS.__eq__)
+        _first_NO_TERMINALS.__ne__ = ne_enum_decorator(_first_NO_TERMINALS.__ne__)
         NO_TERMINALS = enum.Enum('NO_TERMINALS', no_terminals_dict)
         NO_TERMINALS.__bases__ = (_first_NO_TERMINALS, enum.Enum)
         NO_TERMINALS = enum.unique(NO_TERMINALS)
@@ -43,5 +66,5 @@ def get_no_terminals():
     return NO_TERMINALS
 
 
-_first_NO_TERMINALS = None
+_first_NO_TERMINALS: Type[enum.Enum] | None = None
 _NO_TERMINALS = None
--- syntax_analyzer/lexical/terminals.py
+++ syntax_analyzer/lexical/terminals.py
@@ -11,6 +11,27 @@ __all__ = [
 
 ]
 
+
+def eq_enum_decorator(default_eq):
+    def no_terminals_eq(self, item):
+        if isinstance(item, enum.Enum):
+            return item.name == self.name and item.value == self.value
+        else:
+            return default_eq(self, item)
+
+    return no_terminals_eq
+
+
+def ne_enum_decorator(default_ne):
+    def no_terminals_no_eq(self, item):
+        if isinstance(item, enum.Enum):
+            return item.name != self.name or item.value != self.value
+        else:
+            return default_ne(self, item)
+
+    return no_terminals_no_eq
+
+
 keywords = {
     "BEGIN",
     "END",
@@ -24,7 +45,6 @@ keywords = {
     "END",
     "PASS"
 }
-
 
 chars = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_")
 numerals = set("01233456789")
@@ -44,6 +64,13 @@ SPECIAL_CHARS_ENUM = enum.Enum('SPECIAL_CHARS_ENUM', {i: i for i in special_char
 BLANKS_ENUM = enum.Enum('BLANKS_ENUM', {i: i for i in blanks})
 OPTIONAL_BLANKS_ENUM = enum.Enum('OPTIONAL_BLANKS_ENUM', {i: i for i in list(blanks)} | {'<empty>': None})
 
+terminal_enums = {KEYWORDS_ENUM, CHARS_ENUM, NUMERALS_ENUM, UNARY_ENUM, BINARY_ENUM, SPECIAL_CHARS_ENUM, BLANKS_ENUM}
+
+
+
+for enum_cls in terminal_enums:
+    enum_cls.__eq__ = eq_enum_decorator(enum_cls.__eq__)
+    enum_cls.__ne__ = ne_enum_decorator(enum_cls.__ne__)
 
 TERMINALS = DynamicEnum = enum.Enum(
     'TERMINALS',
@@ -54,7 +81,6 @@ TERMINALS = DynamicEnum = enum.Enum(
 
 # class TERMINALS(enum.Enum):
 #     pass
-TERMINALS.__bases__ = ( KEYWORDS_ENUM, CHARS_ENUM, NUMERALS_ENUM, UNARY_ENUM, BINARY_ENUM, SPECIAL_CHARS_ENUM, BLANKS_ENUM, TERMINALS.__bases__[0],)
+TERMINALS.__bases__ = (*terminal_enums, TERMINALS.__bases__[0],)
 
-assert all(map(lambda i: i[0], [(issubclass(TERMINALS, i), i) for i in [KEYWORDS_ENUM, CHARS_ENUM, NUMERALS_ENUM, UNARY_ENUM, BINARY_ENUM, SPECIAL_CHARS_ENUM, BLANKS_ENUM, enum.Enum]]))
-
+assert all(map(lambda i: i[0], [(issubclass(TERMINALS, i), i) for i in [*terminal_enums, enum.Enum]]))
