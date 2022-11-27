8afe5c4aa90b80c739de69d6aaec00da50d85aa9
DaniinXorchenabo <rkbcu@mail.ru>
2022-11-27 15:14:51 +0300

    :sparkles: теперь несколько подряд идущих пробельных символов считаются как один пробельней символ.

--- syntax_analyzer/lexical/lexical_analyzer.py
+++ syntax_analyzer/lexical/lexical_analyzer.py
@@ -1,10 +1,13 @@
-from syntax_analyzer.lexical.terminals import keywords, chars, numerals, unary, binary, special_chars, blanks, TERMINALS
+from syntax_analyzer.lexical.terminals import keywords, chars, numerals, unary, binary, special_chars, blanks, \
+    TERMINALS, BLANKS_ENUM, OPTIONAL_BLANKS_ENUM
 
 
 def lexical_analyzer(data: str):
     # lexem_list = []
+    last_item = OPTIONAL_BLANKS_ENUM(None)
     while bool(data):
         global_item = None
+
         if any(
                 data.startswith(item)
 
@@ -12,7 +15,12 @@ def lexical_analyzer(data: str):
                 for item in dictionary
                 if (global_item := item) is not None
         ):
-            yield  TERMINALS[global_item]
+            if TERMINALS[global_item].name in [i.name for i in BLANKS_ENUM] and last_item.name in [i.name for i in BLANKS_ENUM]:
+                last_item = TERMINALS[global_item]
+                data = data.removeprefix(global_item)
+                continue
+            yield TERMINALS[global_item]
+            last_item = TERMINALS[global_item]
             # lexem_list.append(global_item)
             data = data.removeprefix(global_item)
         else:
